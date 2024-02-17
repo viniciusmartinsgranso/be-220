44cb86577aad66af63ee5850f3abc5ebc1a85f7c
unknown <manut@C-283.labinfo.facens.br>
2024-02-16 23:10:51 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'be-220',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
